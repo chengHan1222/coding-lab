bfe7289f257e99052f299cfa00fc3e1452ab8772
SNinjo <70991369+SNinjo@users.noreply.github.com>
2020-12-27 18:15:57 +0800

    GradesSystem
    the System of manage grades

---
+++ GradesSystemV2/src/GS/Main.java
@@ -0,0 +1,13 @@
+package GS;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Controller(new Model(), new View());
+	}
+	
+	public static void openGSFrame(){
+		new Controller(new Model(), new View());
+	}
+	
+}

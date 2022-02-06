99e05d7daec98d58154fc380e67f79293be4d957
chengHan1222 <75972336+chengHan1222@users.noreply.github.com>
2022-02-06 14:48:14 +0800

    Delete project_cardGame directory

--- project_cardGame/js/Tank.js
+++
@@ -1,9 +0,0 @@
-class Tank extends Ethnicity {
-    constructor(strName, path, intAttack, intBlood, intHolyWater) {
-        super(strName, path, intAttack, intBlood, intHolyWater);
-    }
-    renewCharacterBloodAttack(cardId, loseBlood) {
-        this.intAttack += loseBlood;
-        super.renewCharacterBloodAttack(cardId, loseBlood);
-    }
-}
